a2b1ab18df55b1c25554dea6cca9ef257c6454ab
jgontrum <gontrum@me.com>
2019-07-12 23:41:08 +0200

    Make embedding reading more robust.

--- parseridge/parser/modules/external_embeddings.py
+++ parseridge/parser/modules/external_embeddings.py
@@ -16,17 +16,23 @@ class ExternalEmbeddings(LoggerMixin):
         self.freeze_indices = None
         self.dim = next(iter(self.token_to_embedding.values())).size
 
-    @staticmethod
-    def _load(path, skip_header=False):
+    def _load(self, path, skip_header=False):
         token_to_embedding = {}
 
         with open(path) as embedding_file:
+            i = 1
+
             if skip_header:
                 next(embedding_file)
+                i += 1
 
             for line in embedding_file:
                 token, *embeddings = line.split()
-                token_to_embedding[token] = np.array([float(n) for n in embeddings])
+                try:
+                    token_to_embedding[token] = np.array([float(n) for n in embeddings])
+                except ValueError as e:
+                    self.logger.error(f"Skipping line {i} '{token}': {str(e)}")
+                i += 1
 
         return token_to_embedding
 
