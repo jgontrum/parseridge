7f06b663b9c033e2bdb37f0adfd1f86250ad319e
jgontrum <gontrum@me.com>
2019-07-21 21:44:40 +0200

    Integrate a pre MLP layer

--- parseridge/parser/modules/add_and_norm_layer.py
+++ parseridge/parser/modules/add_and_norm_layer.py
@@ -4,12 +4,12 @@ from parseridge.parser.modules.data_parallel import Module
 
 
 class AddAndNormLayer(Module):
-    def __init__(self, module_size: int, dropout: float = 0.1, **kwargs):
+    def __init__(self, model_size: int, dropout: float = 0.1, **kwargs):
         super().__init__(**kwargs)
-        self.input_size = self.output_size = module_size
+        self.input_size = self.output_size = model_size
 
         self.dropout = nn.Dropout(p=dropout)
-        self.layer_norm = nn.LayerNorm(module_size)
+        self.layer_norm = nn.LayerNorm(model_size)
 
     def forward(self, input: Tensor, output: Tensor):
         return self.layer_norm(input + self.dropout(output))
